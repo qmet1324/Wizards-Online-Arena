67bb1039234a5f829c0f2f9c978e94187ce01bdf
Mikhail Melnik <misha-melnik-03@mail.ru>
2023-02-02 11:48:12 -0500

    Added Pickup Ability on Collision Enter

---
+++ Source/WizardsOnlineArena/MyPickup.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPickup.h"
+#include <Components/BoxComponent.h>
+
+// Sets default values
+AMyPickup::AMyPickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PickupRoot = CreateDefaultSubobject<USceneComponent>(TEXT("PickupRoot"));
+	RootComponent = PickupRoot;
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	PickupMesh->AttachToComponent(PickupRoot, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	PickupBox = CreateDefaultSubobject<UBoxComponent>(TEXT("PickupBox"));
+	PickupBox->SetWorldScale3D(FVector(1.0f, 1.0f, 1.0f));
+	PickupBox->SetGenerateOverlapEvents(true);
+	PickupBox->OnComponentBeginOverlap.AddDynamic(this, &AMyPickup::OnPlayerEnterPickupBox);
+	PickupBox->AttachToComponent(PickupRoot, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+}
+
+// Called when the game starts or when spawned
+void AMyPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMyPickup::OnPlayerEnterPickupBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool fFromSweep, const FHitResult& SweepResult)
+{
+	Destroy();
+}
+
