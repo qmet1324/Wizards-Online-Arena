821463b381be17ee6ce8892fef67504aecbb9679
BUDSTER16 <pwnagemm@hotmail.ca>
2023-03-04 15:46:50 -0500

    Filled out API calls of Network Manager
    Commented the API calls required, needs tweaking but is a great start to setting up matchmaking for the game

--- Source/WizardsOnlineArena/Private/SteamNetworkManager.cpp
+++ Source/WizardsOnlineArena/Private/SteamNetworkManager.cpp
@@ -10,3 +10,48 @@ SteamNetworkManager::SteamNetworkManager()
 SteamNetworkManager::~SteamNetworkManager()
 {
 }
+
+/*
+
+void AddRequestLobbyListFilterSlotsAvailable( int nSlotsAvailable );
+
+void FindMatch()
+{
+	
+	uint_32 lobbies = ISteamMatchmaking::RequestLobbyList()
+	
+	if(lobbies == -1){
+		ISteamMatchmaking::CreateLobby(public, 2);
+	}else
+	{
+		ISteamMatchmaking::JoinLobby(ISteamMatchmaking::GetLobbyByIndex(0));
+	}
+}
+
+void CreatePrivateLobby()
+{
+	ISteamMatchmaking::CreateLobby(private, 2);
+}
+
+void JoinPrivateLobby()
+{
+	int cFriends = SteamFriends()->GetFriendCount( k_EFriendFlagImmediate );
+	for ( int i = 0; i < cFriends; i++ )
+	{
+		FriendGameInfo_t friendGameInfo;
+		CSteamID steamIDFriend = SteamFriends()->GetFriendByIndex( i, k_EFriendFlagImmediate );
+		if ( SteamFriends()->GetFriendGamePlayed( steamIDFriend, &friendGameInfo ) && friendGameInfo.m_steamIDLobby.IsValid() )
+		{
+			// friendGameInfo.m_steamIDLobby is a valid lobby, you can join it or use RequestLobbyData() get its metadata
+		}
+	}
+}
+
+void InviteFriend(CSteamID friendID)
+{
+	if(InviteUserToLobby(steamIDLobby, friendID) != 1)
+	{
+		Log("Friend Declined");
+	}
+}
+*/
