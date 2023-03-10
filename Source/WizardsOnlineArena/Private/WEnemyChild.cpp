ee02e66410b3ac92f3d5e89f88d7af62de5f9ff2
ricklodi <91613337+ricklodi@users.noreply.github.com>
2023-03-10 09:04:39 -0500

    Changed Enemy class
    Put GameMode->Zones[BelongsToZone] -= 1 inside the safety check

--- Source/WizardsOnlineArena/Private/WEnemyChild.cpp
+++ Source/WizardsOnlineArena/Private/WEnemyChild.cpp
@@ -3,10 +3,11 @@
 
 #include "WEnemyChild.h"
 #include "WMainGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 AWEnemyChild::AWEnemyChild()
 {
-
+	GameMode = Cast<AWMainGameMode>(UGameplayStatics::GetGameMode(this));
 }
 
 void AWEnemyChild::Tick(float DeltaTime)
@@ -20,15 +21,16 @@ void AWEnemyChild::OnDeath()
 	{
 		//Creates a GameMode pointer to our AWMainGameMode
 		//It needs to use the function GetWorld()->GetAuthGameMode. So we pass our class as the template
-		AWMainGameMode* GameMode = GetWorld()->GetAuthGameMode<AWMainGameMode>();
+		//AWMainGameMode* GameMode = GetWorld()->GetAuthGameMode<AWMainGameMode>();
+		GameMode = GameMode == nullptr ? GetWorld()->GetAuthGameMode<AWMainGameMode>() : GameMode;
 		if (GameMode != nullptr)	//safety check
 		{
+
+			//Tells the GameMode that this enemy that belonged to the specific zone is dead
+			//BUG WHEN THE OTHER Client kills it
+			GameMode->Zones[BelongsToZone] -= 1;
 			GameMode->PawnKilled(this);	//Calls the PawnKilled function and passes this Pawn as a parameter
 		}
-
-		//Tells the GameMode that this enemy that belonged to the specific zone is dead
-		//BUG WHEN THE OTHER Client kills it
-		GameMode->Zones[BelongsToZone] -= 1;
 
 		isDead = true;
 	}
--- Source/WizardsOnlineArena/Public/WEnemyChild.h
+++ Source/WizardsOnlineArena/Public/WEnemyChild.h
@@ -28,6 +28,10 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		int BelongsToZone;
 
+	UPROPERTY()
+		class AWMainGameMode* GameMode;
+
+
 private:
 
 
